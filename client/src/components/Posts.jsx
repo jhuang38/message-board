2546c74754c9401fea0b05f0fadd5e4a496f9ab2
James Huang <jhuang2003@yahoo.ca>
2022-02-17 17:25:16 -0800

    added date formatting and removed draggability from posts

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -5,15 +5,8 @@ function Posts(props) {
         whileHover: {
             scale: 1.02
         },
-        dragConstraints: {
-            left: 0, 
-            right: 0, 
-            top: 0, 
-            bottom: 0
-        },
         initial: {
             x: '-100vw',
-            opacity: 0
         }
     }
     return (
@@ -21,17 +14,32 @@ function Posts(props) {
             <ul className = "postList">
                 {(!props.posts || props.posts.length === 0)? "No posts yet!" :
                     props.posts.map((post, index) => {
+                        const currDate = new Date(post.date);
+                        const dateString = currDate.toLocaleString();
+                        // remove the seconds from the time measurement
+                        const formattedDateArray = dateString.split(dateString.slice(15, 18));
+                        const formattedDateString = formattedDateArray.join('');
                         return (
                         <motion.li key = {post._id} className = "postElement" 
-                        drag
                         variants={postVariant}
                         initial = "initial"
-                        animate={{x: '0', opacity: 1}}
-                        transition={{duration: 0.8, delay: index * 0.2}}
-                        whileHover = "whileHover">
-                            <h3>{post.title}</h3>
-                            <h4>{post.author}</h4>
-                            <p>{post.textContent}</p>
+                        animate= {{
+                            x: 0,
+                            transition: {
+                                duration: 0.8,
+                                delay: index * 0.2
+                            }
+                        }}
+                        whileHover = "whileHover"
+                        >
+                            <div className = "postContent">
+                                <h3>{post.title}</h3>
+                                <h4>{post.author}</h4>
+                                <p>{post.textContent}</p>
+                            </div>
+
+                            <div className = "postDate">{formattedDateString}</div>
+                            
                         </motion.li>
                         )
                     })
